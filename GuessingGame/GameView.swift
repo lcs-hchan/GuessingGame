06e371e0834dd7486f806e05c8aea9423009668c
Hayden Chan <hchan@lcs.on.ca>
2024-02-02 08:53:17 -0500

    Make Game to be able to type numbers instead lf using stepper

--- GuessingGame/GameView.swift
+++ GuessingGame/GameView.swift
@@ -12,7 +12,8 @@ struct GameView: View {
     // MARK: Stored properties
     
     // What number the user has guessed
-    @State var selectedNumber = 50
+
+    @State var givenInput = ""
     
     // What number the computer wants the user to guess
     @State var target = Int.random(in: 1...100)
@@ -34,13 +35,7 @@ struct GameView: View {
                 Text("Guess what it is!")
                     .font(.headline)
                 
-                Stepper(value: $selectedNumber, in: 1...100) {
-                    HStack {
-                        Text("Make a guess:")
-                        Spacer()
-                        Text("\(selectedNumber)")
-                    }
-                }
+                TextField("Make a guess", text: $givenInput)
                 
                 Button {
                     checkGuess()
@@ -92,6 +87,10 @@ struct GameView: View {
     
     // MARK: Functions
     func checkGuess() {
+        guard let selectedNumber = Int(givenInput) else {
+            feedback = "Please provide an integer."
+            return
+        }
         
         if selectedNumber == target{
             feedback = "you are correct"
@@ -101,7 +100,7 @@ struct GameView: View {
             feedback = "bigger"
         }
         
-        
+     
         // Save the user's guesses
         guessesMade.append(selectedNumber)
         
@@ -111,7 +110,7 @@ struct GameView: View {
     func reset() {
 
         // Start the user back at 50
-        selectedNumber = 50
+        givenInput = ""
         
         // Have the computer guess a new number
         target = Int.random(in: 1...100)
